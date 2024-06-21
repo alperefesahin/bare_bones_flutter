ade4200da9d7250839c21b2362e645cf78ab9767
sahinefe35 <efe.sahin@pera.software>
2024-06-22 02:07:27 +0300

    remove riverpod from original content
    will be created new branches for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:bare_bones_flutter/core/di/dependency_injector.dart';
 import 'package:bare_bones_flutter/core/init/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   injectionSetup();
 
-  runApp(const ProviderScope(child: App()));
+  runApp(const App());
 }
