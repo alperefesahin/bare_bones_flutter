daecd344c888aabcb6e77b9cd576699c59553d55
sahinefe35 <efe.sahin@pera.software>
2024-06-22 23:44:49 +0300

    add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bare_bones_flutter/core/di/dependency_injector.dart';
 import 'package:bare_bones_flutter/core/init/app/app.dart';
+import 'package:bare_bones_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   injectionSetup();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
